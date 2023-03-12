5071de85150e6e7f93a04fa6aff1910aa1472541
Eri Schon <erischon@gmail.com>
2023-03-12 17:35:38 +0100

    skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -17,6 +17,11 @@ const skills = [
         icon: "sass",
       },
       {
+        name: "tailwindcss",
+        link: "https://tailwindcss.com/",
+        icon: "tailwindcss",
+      },
+      {
         name: "JavaScript",
         link: "https://www.w3schools.com/js/",
         icon: "js",
@@ -26,19 +31,76 @@ const skills = [
         link: "https://reactjs.org/",
         icon: "react",
       },
-    ],
-    webSite: [
+
       {
-        name: "HTML",
-        link: "https://www.w3schools.com/html/",
-        icon: "html5",
+        name: "Astro js",
+        link: "https://astro.build/",
+        icon: "astro",
+      },
+    ],
+    fullstack: [
+      {
+        name: "TypeScript",
+        link: "https://www.typescriptlang.org/",
+        icon: "typescript",
+      },
+      {
+        name: "Next.js",
+        link: "https://nextjs.org/",
+        icon: "nextjs",
+      },
+      {
+        name: "tRPC",
+        link: "https://trpc.io/",
+        icon: "trpc",
+      },
+      {
+        name: "GraphQL",
+        link: "https://graphql.org/",
+        icon: "graphql",
+      },
+      {
+        name: "Prisma",
+        link: "https://www.prisma.io/",
+        icon: "prisma",
+      },
+      {
+        name: "postgreSQL",
+        link: "https://www.postgresql.org/",
+        icon: "postgresql",
+      },
+
+      {
+        name: "Node.js",
+        link: "https://nodejs.org/en/",
+        icon: "nodejs",
+      },
+      {
+        name: "Express",
+        link: "https://expressjs.com/",
+        icon: "express",
+      },
+      {
+        name: "Nest.js",
+        link: "https://nestjs.com/",
+        icon: "nestjs",
+      },
+      {
+        name: "MongoDB",
+        link: "https://www.mongodb.com/",
+        icon: "mongodb",
       },
     ],
     other: [
       {
-        name: "HTML",
-        link: "https://www.w3schools.com/html/",
-        icon: "html5",
+        name: "git",
+        link: "https://git-scm.com/",
+        icon: "git",
+      },
+      {
+        name: "GitHub",
+        link: "",
+        icon: "github",
       },
     ],
   },
