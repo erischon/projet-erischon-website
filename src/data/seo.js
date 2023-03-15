5e45eea7fde8a32bd0ef80e099ff262bad446884
Eri Schon <erischon@gmail.com>
2023-03-15 09:29:34 +0100

    added seo

---
+++ src/data/seo.js
@@ -0,0 +1,9 @@
+const seo = {
+  title: "Eri Schön",
+  description: "Développeur Full Stack React, Nextjs, MERN.",
+  url: "https://website.erischon.dev",
+  image: "https://website.erischon.dev/images/erischon_a.png",
+  twitter: "@erischon",
+};
+
+export default seo;
